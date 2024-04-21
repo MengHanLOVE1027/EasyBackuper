4e3f8fd71cbba7bb4629cf81d6f339a77ec8858b
MengHanLOVE1027 <2193438288@qq.com>
2024-04-21 21:42:28 +0800

    为了修改简介只好再发布个新版本了

--- EasyBackuper.js
+++ EasyBackuper.js
@@ -5,8 +5,8 @@
 // 注册插件
 const plugin = {
     Name: "EasyBackuper",
-    Introduction: "简单化的LSE - JS备份插件 v0.2.7 作者: 梦涵LOVE",
-    Version: [0, 2, 7],
+    Introduction: "简单化的LSE - JS备份插件 v0.2.8 作者: 梦涵LOVE",
+    Version: [0, 2, 8],
     Other: {
         Author: "梦涵LOVE",
         Github: "https://github.com/MengHanLOVE1027/EasyBackuper",
@@ -30,7 +30,7 @@ const plugin = {
 
 // 声明常量
 const plugin_name = "EasyBackuper",
-    plugin_version = "v0.2.7",
+    plugin_version = "v0.2.8",
     cmd_name = "backup",
     cmd_alias = "easybackuper",
     plugin_path = `./plugins/${plugin_name}`,
